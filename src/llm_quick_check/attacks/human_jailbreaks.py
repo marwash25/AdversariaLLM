2032846e48b0b16b44c55fa60afbcac96289fc61
Marwa El Halabi <marwa.elhalabi@gmail.com>
2026-04-02 15:55:54 -0400

    Fixing bug in HumanJailbreaksAttack: set N to  JAILBREAKS length instead of original conversations

--- src/llm_quick_check/attacks/human_jailbreaks.py
+++ src/llm_quick_check/attacks/human_jailbreaks.py
@@ -98,7 +98,7 @@ class HumanJailbreaksAttack(Attack):
         shifted_target_tensors_flat = [t.roll(-1, 0) for t in full_token_tensors_list_flat]
 
         B = len(original_conversations)
-        N = len(original_conversations[0]) #M: so always 2 here (see assert above: len(conversation) == 2)
+        N = len(JAILBREAKS)
 
         # --- 2. Calculate Losses ---
         t_start_loss = time.time()
