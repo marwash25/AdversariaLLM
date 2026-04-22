2bdf3aec2c652dd7934444f239964c20bdacdcc5
Marwa El Halabi <marwa.elhalabi@gmail.com>
2026-04-22 18:40:04 -0400

    added separate functions to do map from sets to int vectors and back and corrected bug in the implementation of this map that was in set_function_reduction

--- src/llm_quick_check/attacks/dummy.py
+++ src/llm_quick_check/attacks/dummy.py
@@ -4,7 +4,7 @@ import time
 import logging
 import sys
 from tqdm import trange
-from typing import Dict, List, Optional, Tuple, Callable, Any
+from typing import List, Optional, Tuple, Callable, Any
 import torch
 from math import log2
 from torch import Tensor
@@ -44,9 +44,10 @@ def _masked_cross_entropy(
 
     Args:
         shift_logits: logits of shape (batch_size, seq_len - 1, vocab_size)
-        shift_labels: labels of shape (batch_size, seq_len - 1)
-        logits of token i predicts label i (token i+1)
-        logit_mask: mask where to apply loss to of shape (seq_len -1,), shared across the batch
+        shift_labels: labels of shape (batch_size, seq_len - 1); logits of token
+            i predicts label i (token i+1)
+        logit_mask: mask where to apply loss to of shape (seq_len -1,),
+            shared across the batch
 
     Returns:
         loss: Tensor of shape (batch_size,)
@@ -73,16 +74,20 @@ def compute_loss(
     attack_mask: torch.BoolTensor,
     lm_reg_weight: float = 0.0,
 ) -> Tuple[Tensor, int]:
-    """Computes the cross-entropy loss on target tokens (-log p(y|q,x)) plus language-model regularizer
-    lm_reg_weight * cross-entropy loss on attack tokens (-log p(x|q))
+    """Computes the cross-entropy loss on target tokens (-log p(y|q,x)) plus
+    language-model regularizer lm_reg_weight * cross-entropy loss on attack
+    tokens (-log p(x|q))
 
     Args:
         model: PreTrainedModel
-        attack_ids: the attack token ids to evaluate. Tensor of shape (batch_size, n_optim_tokens)
-        original_tokens: token ids for the full conversation. Tensor of shape (seq_len,)
-        target_mask: bool mask of shape (seq_len,); True on tokens to apply loss to (target tokens shifted by one to the left).
+        attack_ids: the attack token ids to evaluate. Tensor of shape
+            (batch_size, n_optim_tokens)
+        original_tokens: token ids for the full conversation. Tensor of shape
+            (seq_len,)
+        target_mask: bool mask of shape (seq_len,); True on tokens to apply
+            loss to (target tokens shifted by one to the left).
         #TODO: maybe simpler to not shift target_mask earlier
-        attack_mask: bool mask of shape (seq_len,); True on attack tokens. 
+        attack_mask: bool mask of shape (seq_len,); True on attack tokens.
         lm_reg_weight: Multiplier for the language-model regularizer.
 
     Returns:
@@ -116,56 +121,94 @@ def compute_loss(
     
 # TODO: move SubmodularSetFnReduction to separate file
 class SubmodularSetFnReduction:
-    """Given a DR-submodular discrete function F: V^n -> R, where V = {0, 1, ..., k - 1} and k = 2^t,
-    provides reduction to a submodular set function F_set: 2^([n] x [t]) -> R and its Lovasz extension subgradient computation.
+    """Given a DR-submodular discrete function F: V^n -> R, where V = {0, 1,..., k - 1} 
+    and k = 2^t, provides reduction to a submodular set function F_set: 2^([n] x [t]) -> R 
+    and its Lovasz extension subgradient computation.
+    Reduction from F to F_set is done via a binary representation map, i.e.,
+    F_set(S) = F(M(S)), where X = J_S is the matrix with 1 at indices in S, 0
+    elsewhere, and x = M(S) is the integer vector such that each x_i is the int
+    with binary representation X[i, :].
+    Least significant bit is at column index 0 (bit index matches column index).
+    For simplicity, we represent S by separate rows and cols indices instead of
+    a set of tupples, i.e., S = {(rows[i], cols[i]) for i in
+    range(rows.shape(0))}. # TODO: modify this if needed
     """
-    def __init__(self, F_batch: Callable[[Tensor], [Tensor, int]], k: int, n: int, device: torch.device):
+    def __init__(self, F_batch: Callable[[Tensor], Tuple[Tensor, int]], k: int, n: int, device: torch.device):
         # F_batch should be a function that takes a batch of inputs in V^n (Tensor of shape (batch_size, n)) 
         # and returns the values of F for each input (Tensor of shape (batch_size,)) and flop count (int).
         self.F_batch = F_batch
         self.device = device 
         self.k = k
         self.n = n
-        self.F_set_batch = self._set_function_reduction()
-        # TODO: normalize F(emptyset) = 0
-        
-    def __call__(self, rows: Tensor, cols: Tensor) -> Tensor:
-        return self.F_set_batch(rows, cols)
-
-    def _set_function_reduction(self) -> Callable[[Tensor, Tensor], [Tensor, int]]:
-        """Reduction from F to F_set using binary representation of subsets, i.e.,
-        F_set(S) = F(x), where X = J_S is the matrix with 1 at indices in S, 0 elsewhere, 
-        and x is the vector such that each x_i is the int with binary representation X[i, :]
-        Least significant bit is at column index 0 (bit index matches column index).
-        For simplicity, will use rows and cols indices as input to F_set instead of a set of tupples
-        # TODO: modify this if needed
-        """
-        # TODO: test this even if not used so far. For now assume vocab_size is a power of 2
         self.t = int(log2(self.k))
         assert self.k == 2 ** self.t, "k must be a power of 2"
         self.powers = (1 << torch.arange(self.t, dtype=torch.long, device=self.device)) # more efficient than 2**torch.arange(t)
         
-        def F_set_batch(rows: Tensor, cols: Tensor) -> Tensor:
-            """Compute F_set(S^i) for the sets S^i = {(rows[i, j], cols[i, j]) for j in range(rows.shape(1))}
-            Args:
-                rows: Tensor of shape (batch_size, n x t)
-                cols: Tensor of shape (batch_size, n x t)
-            #TODO: this doesn't check if (row, col) pairs are unique (so true set). Add this check, 
-            # or modify input to be sets of indices in [n x t] which can easily check for uniqueness before 
-            # splitting into rows and cols. For now we don't actualy use this function, so will decide depending on usage.
-            """
-            assert rows.shape == cols.shape, "rows and cols must have the same shape"
-            x = torch.zeros((rows.shape[0], self.n), dtype=torch.long, device=self.device)
-            x.scatter_add_(1, rows, self.powers[cols]) # x[i, rows[i, j]] += powers[cols[i, j]] for all i, j
+        self.F_set_batch = self._set_function_reduction()
+        # TODO: normalize F(emptyset) = 0
+        
+    def __call__(self, rows_list: List[Tensor], cols_list: List[Tensor]) -> Tuple[Tensor, int]:
+        return self.F_set_batch(rows_list, cols_list)
+
+    def bitset2int(self, rows_list: List[Tensor], cols_list: List[Tensor]) -> Tensor:
+        """Implement batched version of map M: 2^([n] x [t]) -> V^n: Convert a list of
+        subsets of [n] x [t] represented by rows and cols indices (List of 1DTensors of
+        length <= n x t) to a batch of integer vectors in V^n (Tensor of shape
+        (batch_size, n))."""
+        #TODO: this doesn't check if (row, col) pairs are unique (so true set). Add this check, 
+        # or modify input to be sets of indices in [n x t] which can easily check for uniqueness before 
+        # splitting into rows and cols. For now we don't actualy use this function, so will decide depending on usage.
+        #TODO: might be more efficient to take as input batch_idx, rows, cols instead, but for now will keep this 
+        # simpler implementation, as I am not actually sure we'll use this for more than one set in the batch.
+        # Same for int2bitset and F_set_batch.
+        assert len(rows_list) == len(cols_list), "rows_list and cols_list must have the same length"
+        assert all(rows_list[i].device == self.device and cols_list[i].device == self.device \
+        for i in range(len(rows_list))), \
+        "all rows_list and cols_list must be on the same device and have the same length"
+
+        x = torch.zeros((len(rows_list), self.n), dtype=torch.long, device=self.device)
+        for i, (rows, cols) in enumerate(zip(rows_list, cols_list)):
+            assert rows.shape[0] == cols.shape[0], "rows and cols must have the same length"
+            x[i].index_add_(0, rows, self.powers[cols]) # x[i, rows[j]] += powers[cols[j]] for all j
+        return x
+
+    def int2bitset(self, x: Tensor) -> Tuple[List[Tensor], List[Tensor]]:
+        """Implement batched version of inverse map M^{-1}: V^n -> 2^([n] x [t]): 
+        Convert a batch of integer vectors in V^n (Tensor of shape (batch_size, n))
+        to a list of subsets of [n] x [t] represented by rows and cols indices (List of 1DTensors of
+        length <= n x t))."""
+        #TODO: adjust implementation if bitset2int is changed
+        assert x.dim() == 2 and x.shape[1] == self.n, "x must be (batch_size, n)"
+        assert x.dtype == torch.long, "x must be of type long"
+        idx = torch.arange(self.t, dtype=torch.long, device=x.device)
+        bits = ((x.unsqueeze(-1) >> idx) & 1).bool() # (batch_size, n, t)
+        batch_idx, rows, cols = bits.nonzero(as_tuple=True)
+        rows_list = []
+        cols_list = []
+        for i in range(bits.size(0)):
+            batch_mask = batch_idx == i
+            rows_list.append(rows[batch_mask])
+            cols_list.append(cols[batch_mask])
+        return rows_list, cols_list
+
+    def _set_function_reduction(self) -> Callable[[List[Tensor], List[Tensor]], Tuple[Tensor, int]]:
+        #TODO: adjust implementation if bitset2int is changed
+        def F_set_batch(rows_list: List[Tensor], cols_list: List[Tensor]) -> Tuple[Tensor, int]:
+            """Implement batched version of F_set: 2^([n] x [t]) -> R: Compute
+            F_set(S^j) for the set S^j = {(rows_list[j][i], cols_list[j][i]) for i in
+            range(rows_list[j].shape[0])}"""
+            x = self.bitset2int(rows_list, cols_list)
             return self.F_batch(x)
 
         return F_set_batch
 
     def subgradient_lovasz_extension(self, X: Tensor, tie_breaker: Optional[Tensor] = None):
-        """Compute a subgradient of the Lovasz extension of self.F_set using Edmonds' greedy algorithm.
+        """Compute a subgradient of the Lovasz extension of self.F_set using
+        Edmonds' greedy algorithm.
         Args:
-            X: Tensor of shape (n, t) in [0,1]^n x t 
-            tie_breaker: Tensor of shape (n, t) used to break ties when sorting X.flatten(). If not provided, original order is used.
+            X: Tensor of shape (n, t) in [0,1]^n x t
+            tie_breaker: Tensor of shape (n, t) used to break ties when sorting
+                X.flatten(). If not provided, original order is used.
         Returns:
             subgradient: Tensor of shape (n, t)
         """
@@ -189,9 +232,10 @@ class SubmodularSetFnReduction:
         # compute F(x^i) for all x^i's
         Fvalues, _ = self.F_batch(x_chain) # TODO: add flop count handling here
 
-        # compute subgradient g_i = F(x^i) - F(x^{i-1})
-        subgradient = torch.diff(Fvalues, prepend=0)
-        subgradient[sorted_idx] = subgradient
+        # compute subgradient g_i = F(x^i) - F(x^{i-1}), assume F(0) = 0
+        subgradient = torch.zeros_like(Fvalues) # (n x t,)
+        subgradient[sorted_idx] = torch.diff(Fvalues, prepend=torch.zeros_like(Fvalues[0]))
+        subgradient = subgradient.view_as(X) # (n, t)
 
         return subgradient, Fvalues, sorted_idx 
 
@@ -220,7 +264,7 @@ class DummyAttack(Attack):
         logging.info(f"Number of embeddings: {num_embeddings}, Tokenizer vocab size: {len(tokenizer)}") # to check if they match
 
         runs = []
-        for idx, conversation in enumerate(dataset):
+        for idx, conversation in enumerate(conversations):
             runs.append(self._attack_single_conversation(model, tokenizer, conversation, tokens[idx], attack_masks[idx], target_masks[idx]))     
            
         return AttackResult(runs=runs)
@@ -239,10 +283,8 @@ class DummyAttack(Attack):
         tokens = tokens.to(device) 
         attack_mask = attack_mask.to(device)
         target_mask = target_mask.to(device)
-        
-        # let's first test F_batch on its own
         F_batch = lambda attack_ids: compute_loss(model, attack_ids, tokens, target_mask, attack_mask, self.config.lm_reg_weight)
-        # F_set_batch = SubmodularSetFnReduction(F_batch, self.vocab_size, tokens.shape[1], device)
+        F_set_batch = SubmodularSetFnReduction(F_batch, self.vocab_size, tokens.shape[1], device)
 
         losses = []
         times = []
@@ -251,6 +293,19 @@ class DummyAttack(Attack):
         # Initialize with the token ids of optim_str_init
         optim_ids = tokens[attack_mask].detach().clone()
 
+        # Test correctness of int2bitset and bitset2int
+        rows, cols = F_set_batch.int2bitset(optim_ids)
+        optim_ids_2 = F_set_batch.bitset2int(rows, cols)
+        assert torch.all(optim_ids == optim_ids_2), "int2bitset and bitset2int are not inverses"
+        logging.info("int2bitset and bitset2int are correctly implemented as inverses")
+
+        # Test correctness of F_set_batch
+        init_loss, init_flops = F_batch(optim_ids)
+        init_loss_2, init_flops_2 = F_set_batch(rows, cols)
+        assert torch.all(init_loss == init_loss_2), "F_batch and F_set_batch are not consistent"
+        logging.info("F_batch and F_set_batch are consistent")
+
+        logging.info(f"Initial loss: {init_loss}, Initial flops: {init_flops}")
         for i in (pbar := trange(self.config.num_steps, file=sys.stdout)):
             current_loss, time_for_step, optim_ids, optim_str, flops_for_step = self._single_step(optim_ids, F_batch)
             losses.append(current_loss)
@@ -321,8 +376,10 @@ class DummyAttack(Attack):
     def _single_step(self, optim_ids: Tensor, F_batch: Callable[[Tensor], Any]) -> Tuple[float, float, torch.Tensor, str, int]:
         """ Single step of the attack.
         Args:
-            optim_ids: Current attack token ids. Tensor of shape (n_optim_tokens,)
-            F_batch: Function that computes the loss for a batch of attack token ids.
+            optim_ids: Current attack token ids. Tensor of shape
+                (n_optim_tokens,)
+            F_batch: Function that computes the loss for a batch of attack token
+                ids.
         """
       
         t_start_step = time.time()
