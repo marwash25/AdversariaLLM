94f231a1860bc4dfcfdf6bd4d9dbb640514366fe
Marwa El Halabi <marwa.elhalabi@gmail.com>
2026-05-13 17:04:34 -0400

    temporarily setting L=100 when normalizing in PGM to start with a smaller step size when initializing with GCG sol

--- src/llm_quick_check/attacks/submodular_utils/dsm_optimizers.py
+++ src/llm_quick_check/attacks/submodular_utils/dsm_optimizers.py
@@ -90,7 +90,7 @@ def pgm_lovasz(F_set_batch: SetFnReduction, x_init: Tensor, num_steps: int, L: f
             L = max(L, 1e-12) # L < 1e-12 shouldn't happen unless F = 0 but just in case
         elif L == "normalize":
             normalize = True
-            L = 1.0
+            L = 100.0 #just testing what happens if we use smaller step size when initializing from GCG sol
         else:
             raise ValueError("If L is a string, it must be either 'singletons' or 'normalize'.")
 
